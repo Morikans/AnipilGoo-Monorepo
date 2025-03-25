4e7250c5083e6100192bc655052bd2e9868ca6fe
morikan <shirousagi.morikan@gmail.com>
2025-03-26 06:35:27 +0900

    ログアウト機能

--- packages/backend/dist/controllers/auth/index.js
+++ packages/backend/dist/controllers/auth/index.js
@@ -16,3 +16,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./signupController"), exports);
 __exportStar(require("./loginController"), exports);
+__exportStar(require("./logoutController"), exports);
--- packages/backend/dist/routes/authRoutes.js
+++ packages/backend/dist/routes/authRoutes.js
@@ -8,4 +8,5 @@ const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 router.post("/signup", controllers_1.signup);
 router.post("/login", controllers_1.login);
+router.post("/logout", controllers_1.logout);
 exports.default = router;
