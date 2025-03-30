641eade020d6ca30ec8571f936d868fe1103cb53
morikans <shirousagi.morikan@gmail.com>
2025-03-30 09:26:25 +0900

    authForm修正

--- packages/frontend/src/app/components/common/GoogleButton.tsx
+++ packages/frontend/src/app/components/common/GoogleButton.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const GoogleButton = ({ text }: Props) => {
   return (
-    <div className="w-full border border-gray-300 rounded-sm p-3 flex gap-2 items-center justify-center mt-7 cursor-pointer hover:bg-gray-100">
+    <div className="w-full border border-gray-300 rounded-sm p-3 flex gap-2 items-center justify-center cursor-pointer hover:bg-gray-100">
       <img src="/google_logo.svg" alt="" className="w-8" />
       <p>{text}</p>
     </div>
--- packages/frontend/src/app/components/feature/AuthForm.tsx
+++ packages/frontend/src/app/components/feature/AuthForm.tsx
@@ -40,51 +40,55 @@ export const AuthForm = ({ formType }: ButtonProps) => {
   return (
     <div className="mx-auto max-w-[34rem]">
       <h2 className="font-bold text-2xl text-center">{title}</h2>
-      <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-10 mt-8">
-        <Input
-          id="email"
-          text="メールアドレス"
-          type="email"
-          name="email"
-          register={register}
-          validation={{
-            required: "メールアドレスは必須です",
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "正しいメールアドレスを入力してください",
-            },
-          }}
-          error={errors.email?.message}
-        />
-
-        <div className="mt-7 flex items-center">
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-white mt-8 py-2 border border-gray-300">
+        <div className="p-10">
           <Input
-            id="password"
-            text="パスワード"
-            type="password"
-            name="password"
+            id="email"
+            text="メールアドレス"
+            type="email"
+            name="email"
             register={register}
             validation={{
-              required: "パスワードは必須です",
-              minLength: {
-                value: 6,
-                message: "パスワードは6文字以上で入力してください",
+              required: "メールアドレスは必須です",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "正しいメールアドレスを入力してください",
               },
             }}
-            error={errors.password?.message}
-            mask={true}
+            error={errors.email?.message}
           />
+
+          <div className="mt-7 flex items-center">
+            <Input
+              id="password"
+              text="パスワード"
+              type="password"
+              name="password"
+              register={register}
+              validation={{
+                required: "パスワードは必須です",
+                minLength: {
+                  value: 6,
+                  message: "パスワードは6文字以上で入力してください",
+                },
+              }}
+              error={errors.password?.message}
+              mask={true}
+            />
+          </div>
+          <div className="mt-7">
+            <Button text={title} btnColor="blown" />
+          </div>
         </div>
-        <GoogleButton text={`Googleでログイン`} />
-        <div className="mt-7">
-          <Button text={title} btnColor="blown" />
-        </div>
-        <div className="block mt-7 text-center">
-          <Link href={linkHref}>
-            <p className="border-b-1 inline hover:opacity-80 cursor-pointer">
-              {linkText}
-            </p>
-          </Link>
+        <div className="border-t-1 border-gray-300 p-10">
+          <GoogleButton text={`Googleでログイン`} />
+          <div className="block mt-7 text-center">
+            <Link href={linkHref}>
+              <p className="border-b-1 inline hover:opacity-80 cursor-pointer">
+                {linkText}
+              </p>
+            </Link>
+          </div>
         </div>
       </form>
     </div>
