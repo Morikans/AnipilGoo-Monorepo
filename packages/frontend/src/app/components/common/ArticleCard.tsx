e7febe1a4ffdd4faf613ccdaf415b3a72952267f
morikans <shirousagi.morikan@gmail.com>
2025-04-01 02:19:29 +0900

    dummy作成

--- packages/frontend/src/app/components/common/ArticleCard.tsx
+++ packages/frontend/src/app/components/common/ArticleCard.tsx
@@ -4,27 +4,36 @@ import Link from "next/link";
 import { FaMapMarkerAlt, FaRegHeart, FaRegComment } from "react-icons/fa";
 import { daysAgoConvert } from "@/utils";
 
-interface CardProps {
-  article: Article;
+export interface ArticleType extends Article {
   user: User;
 }
 
-export const ArticleCard = ({ article, user }: CardProps) => {
+export const ArticleCard = ({ article }: { article: ArticleType }) => {
   console.log(article.createdAt);
   return (
-    <Link href={`/article/${article.id}`}>
-      <p>{article.animeName}</p>
-      <img src={article.thumbnailImageURL} alt="" />
-      <p>{article.title}</p>
-      <FaMapMarkerAlt />
-      <p>
-        {article.prefectureName} {article.cityName}
-      </p>
-      <img src={user.imageURL} alt="" />
-      <p>{user.name}</p>
-      <p>{article.likesCount}</p>
-      <p>{article.commentsCount}</p>
-      <p>{daysAgoConvert(article.createdAt)}</p>
-    </Link>
+    <div className="rounded bg-secondary">
+      <Link href={`/article/${article.id}`}>
+        <p className="py-1 px-2 text-white">{article.animeName}</p>
+        <img
+          src={article.thumbnailImageURL}
+          alt=""
+          className="aspect-video object-cover"
+        />
+        <div className="bg-white p-2">
+          <p>{article.title}</p>
+          <div className="flex content-center gap-1">
+            <FaMapMarkerAlt size={20} />
+            <p>
+              {article.prefectureName} {article.cityName}
+            </p>
+          </div>
+          <img src={article.user.imageURL} alt="" />
+          <p>{article.user.name}</p>
+          <p>{article.likesCount}</p>
+          <p>{article.commentsCount}</p>
+          <p>{daysAgoConvert(article.createdAt)}</p>
+        </div>
+      </Link>
+    </div>
   );
 };
--- packages/frontend/src/app/components/feature/HomeArticles.tsx
+++ packages/frontend/src/app/components/feature/HomeArticles.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ArticleCard, SubTitle } from "../common";
+import { createRandomArticles } from "@/dummyData/dummyArticles";
 
 interface Props {
   type:
@@ -10,36 +11,13 @@ interface Props {
 }
 
 export const HomeArticles = ({ type }: Props) => {
-  const article = {
-    title: "string",
-    id: 1,
-    animeName: "string",
-    content: "string",
-    thumbnailImageURL: "string",
-    imageURLs: ["a"],
-    clickCount: 1,
-    createdAt: new Date('2024-11-12'),
-    updatedAt: new Date(),
-    likesCount: 1,
-    commentsCount: 1,
-    prefectureName: "string",
-    cityName: "string",
-    userId: "string",
-  };
-  const user = {
-    email: "string",
-    id: "string",
-    name: "string",
-    createdAt: new Date(),
-    imageURL: "string",
-  };
+  const articles = createRandomArticles(3);
   return (
     <div>
       <SubTitle type={type} />
-      <ArticleCard
-        article={article}
-        user={user}
-      />
+      {articles.map((article) => (
+        <ArticleCard article={article} />
+      ))}
     </div>
   );
 };
